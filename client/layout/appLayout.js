f2daad321de88c25e288608ab46ca68c2bde64bc
Joseph <illumeweb@gmail.com>
2018-05-31 19:42:50 +0900

    minor changes

--- client/layout/appLayout.js
+++ client/layout/appLayout.js
@@ -14,6 +14,18 @@ Template.loginList.helpers({
     return Meteor.user().emails[0].address;
   } 
 });
+Template.loginPage.helpers({
+  
+  firstName: function() {
+    return Meteor.user().profile.firstName;
+  },
+      lastName: function() {
+    return Meteor.user().profile.lastName;
+  },
+   userEmail: function() {
+    return Meteor.user().emails[0].address;
+  } 
+});
 Template.header.helpers({
   
   firstName: function() {
